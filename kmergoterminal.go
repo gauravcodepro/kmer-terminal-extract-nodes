3701f982b62410568155849dc76143c625e84c64
Gaurav Sablok <gauravcodepro@gmail.com>
2024-08-05 20:17:59 +0200

    Update kmergoterminal.go
    added updated READ description

--- kmergoterminal.go
+++ kmergoterminal.go
@@ -3,7 +3,8 @@
 // Date 2024-8-5
 /*
  A Go program for filtering of the kmers that are specific to the terminal ends. It will first create the kmers and then will give 
- out those kmers for which the terminal ends are mapping to the specific for implementation into the graph edges. 
+ out those kmers for which the terminal ends are mapping to the specific for implementation into the graph edges. You can change the value of 2 to any kmer specific. 
+ I am building a go package as a part of go learning which will have all these including a desktop application, written in GO.
 */
 
 package main 
